a47370662ba102b29140ea1aecca483632f8ddc1
Nguyen Hoang Duy Linh <yudlinh0810@gmail.com>
2025-04-16 18:41:26 +0700

    feat: add ManageCar page with full CRU and backend integration

--- fe/admin/src/App.tsx
+++ fe/admin/src/App.tsx
@@ -6,8 +6,15 @@ import "./styles/app.scss";
 import "./styles/reset.scss";
 import "./styles/root.scss";
 import PrivateRoute from "./components/PrivateRoute";
+import ManageLayout from "./layouts/ManageLayout";
+import ManageCar from "./pages/ManageCar/ManageCar";
+import DetailCar from "./pages/ManageCar/DetailCar";
+import AddCar from "./pages/ManageCar/AddCar";
+import UpdateCar from "./pages/ManageCar/UpdateCar";
+import useClientWidth from "./utils/useClientWidth.util";
 
 function App() {
+  useClientWidth();
   return (
     <>
       <Router>
@@ -16,6 +23,15 @@ function App() {
             {/* Route bảo vệ */}
             <Route element={<PrivateRoute />}>
               <Route index element={<div>Home</div>} />
+
+              {/* Car Manage */}
+              <Route path="/car-manage" element={<ManageLayout />}>
+                <Route index element={<ManageCar />} />
+                <Route path="page/:page" element={<ManageCar />} />
+                <Route path="detail/:id" element={<DetailCar />} />
+                <Route path="add" element={<AddCar />} />
+                <Route path="update/:id" element={<UpdateCar />} />
+              </Route>
             </Route>
           </Routes>
         </Layout>
---
+++ fe/admin/src/pages/ManageCar/AddCar.tsx
@@ -0,0 +1,122 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ImageListCUD, { ImageCUDType } from "../../components/ImageListCUD";
+import { useCustomNavMutation } from "../../hooks/useCustomQuery";
+import { addCar } from "../../services/car.service";
+import styles from "../../styles/addCar.module.scss";
+
+const AddCar = () => {
+  const [images, setImages] = useState<ImageCUDType[]>([]);
+
+  const [form, setForm] = useState({
+    licensePlate: "",
+    capacity: 0,
+    type: "",
+    indexIsMain: "",
+  });
+
+  const mutateUpdate = useCustomNavMutation(
+    addCar,
+    "/car-manage",
+    "Thêm xe thành công",
+    "Thêm xe thất bại"
+  );
+
+  // const imageList = car?.images ?? null;
+
+  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+  };
+
+  const handleAddCar = async () => {
+    const formData = new FormData();
+    formData.append("data", JSON.stringify(form));
+
+    images.forEach((image, index) => {
+      formData.append("files", image.image);
+      if (image.isMain) {
+        const updateForm = { ...form };
+        updateForm.indexIsMain = index.toString();
+        formData.set("data", JSON.stringify(updateForm));
+      }
+    });
+
+    mutateUpdate.mutate(formData);
+  };
+
+  useEffect(() => {}, [images]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles["feats"]}>
+        <Link to={`/car-manage`} className={`${styles["btn-back"]} ${styles.btn}`}>
+          Quay lại
+        </Link>
+        <button className={`${styles["btn-delete"]} ${styles.btn}`}>Xóa</button>
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(); // Ngăn không cho form submit mặc định
+          handleAddCar();
+        }}
+        className={styles["update-car"]}
+      >
+        <div className={styles.title}>
+          <h2 className={styles["content-title"]}>Thông tin chi tiết</h2>
+        </div>
+        <ul className={styles["input-list"]}>
+          {[
+            { label: "Biển số xe", name: "licensePlate" },
+            { label: "Sức chứa", name: "capacity" },
+            { label: "Loại xe", type: "select", name: "type" },
+          ].map((item, index) => (
+            <li key={index} className={` ${styles.item}`}>
+              <label htmlFor={item.label} className={styles.title}>
+                {item.label}
+              </label>
+
+              {item.type !== "select" ? (
+                <input
+                  className={styles.input}
+                  id={item.label}
+                  name={item.name}
+                  onChange={handleChangeValue}
+                />
+              ) : (
+                <select
+                  name={item.name}
+                  id={item.label}
+                  className={styles.input}
+                  onChange={handleChangeValue}
+                >
+                  <option>--- Chọn loại xe ---</option>
+                  {["xe thường", "xe giường nằm"].map((type) => (
+                    <option key={type} value={type}>
+                      {type}
+                    </option>
+                  ))}
+                </select>
+              )}
+            </li>
+          ))}
+          <li className={`${styles.item} ${styles["action-img"]}`}>
+            <p className={`${styles.title}`}>Hình ảnh</p>
+            <div className={`${styles["img-list"]}`}>
+              <div className={`${styles["img-item"]} ${styles["add-img"]}`}>
+                <ImageListCUD images={images} setImages={setImages} />
+              </div>
+            </div>
+          </li>
+          <div className={styles.action}>
+            <button type="submit" className={styles["btn-add"]}>
+              Thêm xe
+            </button>
+          </div>
+        </ul>
+      </form>
+    </div>
+  );
+};
+
+export default AddCar;
