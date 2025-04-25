fc7030cb87b1a4451be4ffaef5f43535a4f93e9d
Nguyen Hoang Duy Linh <yudlinh0810@gmail.com>
2025-04-25 22:34:57 +0700

    refactor: convert Car function to class export

--- be/service/src/routes/car.routes.ts
+++ be/service/src/routes/car.routes.ts
@@ -1,28 +1,5 @@
 import express from "express";
-import {
-  addCarCTL,
-  addImgCarCTL,
-  deleteCarCTL,
-  deleteImgCarCTL,
-  getAllCarCTL,
-  getCarByIdCTL,
-  getCarByLicensePlateCTL,
-  getCarByStatusCTL,
-  getCarByTypeAndStatusCTL,
-  getCarByTypeCTL,
-  updateCarCTL,
-  updateImgCarCTL,
-} from "../controllers/car.controller";
-import {
-  validateCreateCar,
-  validateCreateCarMiddleware,
-  validateUpdateCar,
-  validateUpdateCarMiddleware,
-} from "../middlewares/car.middleware";
-import { validateToken } from "../middlewares/user.middleware";
-import { RequestWithCar } from "../@types/car.type";
-import { successResponse } from "../utils/response.util";
-import { CarStatus, CarType } from "../@types/car.type";
+
 import {
   uploadImages,
   uploadImagesToCloudinary,
@@ -30,15 +7,20 @@ import {
 } from "../middlewares/uploadHandler";
 import pool from "../config/database";
 import { uploadImage } from "../middlewares/multerConfig";
+import { verifyAccessToken } from "../services/auth.service";
+import { authorizeRoles } from "../middlewares/auth.middleware";
+import { CarController } from "../controllers/car.controller";
 
 const carRouter = express.Router();
+const carController = new CarController();
+
 carRouter.post(
   "/add",
   // validateCreateCar,
   // validateCreateCarMiddleware,
   uploadImages,
   uploadImagesToCloudinary,
-  addCarCTL
+  carController.addCar
 );
 carRouter.put(
   "/update",
@@ -46,33 +28,58 @@ carRouter.put(
   // validateUpdateCarMiddleware,
   uploadImages,
   uploadImagesToCloudinary,
-  updateCarCTL
+  carController.updateCar
 );
-carRouter.post("/add-img", addImgCarCTL);
-carRouter.put("/image/update", uploadImage, uploadImageToCloudinary, updateImgCarCTL);
-carRouter.delete("/image/delete", deleteImgCarCTL);
-carRouter.delete("/delete/:id", deleteCarCTL);
-carRouter.get("/get-all", getAllCarCTL);
-carRouter.post("/detail/:id", getCarByIdCTL);
-carRouter.get("/license-plate/:licensePlate", getCarByLicensePlateCTL);
-carRouter.post("/type/:type", getCarByTypeCTL);
-carRouter.post("/status/:status", getCarByStatusCTL);
-carRouter.post("/type/:type/status/:status", getCarByTypeAndStatusCTL);
-carRouter.get("/get", async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT NOW()"); // Kiểm tra kết nối bằng truy vấn thời gian hiện tại
-    client.release();
+// carRouter.post("/add-img", verifyAccessToken, authorizeRoles("admin"), carController.addImageCar);
+carRouter.put(
+  "/image/update",
+  verifyAccessToken,
+  authorizeRoles("admin"),
+  uploadImage,
+  uploadImageToCloudinary,
+  carController.updateImgCar
+);
+carRouter.delete(
+  "/image/delete",
+  verifyAccessToken,
+  authorizeRoles("admin"),
+  carController.deleteImgCar
+);
+carRouter.delete(
+  "/delete/:id",
+  verifyAccessToken,
+  authorizeRoles("admin"),
+  carController.deleteCar
+);
+carRouter.get("/get-all", verifyAccessToken, authorizeRoles("admin"), carController.getAllCar);
+// carRouter.get(
+//   "/detail/:id",
+//   verifyAccessToken,
+//   authorizeRoles("admin", "customer"),
+//   carController.getCarById
+// );
+carRouter.get(
+  "/detail/:licensePlate",
+  verifyAccessToken,
+  authorizeRoles("admin", "customer"),
+  carController.getCarByLicensePlate
+);
 
-    res.json({
-      message: "Connected to PostgreSQL!",
-      serverTime: result.rows[0].now,
-    });
-  } catch (err) {
-    console.error("Connection error:", err);
-    res.status(500).json({ message: "Database connection failed", error: err });
-  }
-});
+// carRouter.get("/get", async (req, res) => {
+//   try {
+//     const client = await pool.connect();
+//     const result = await client.query("SELECT NOW()"); // Kiểm tra kết nối bằng truy vấn thời gian hiện tại
+//     client.release();
+
+//     res.json({
+//       message: "Connected to PostgreSQL!",
+//       serverTime: result.rows[0].now,
+//     });
+//   } catch (err) {
+//     console.error("Connection error:", err);
+//     res.status(500).json({ message: "Database connection failed", error: err });
+//   }
+// });
 
 export default carRouter;
 // Compare this snippet from be-booktickets/src/controllers/car.controller.ts:
