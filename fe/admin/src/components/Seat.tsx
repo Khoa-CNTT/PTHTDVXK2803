fe7cbf57214ab1831bbc79772d48c6a4b79c828f
Nguyen Hoang Duy Linh <yudlinh0810@gmail.com>
2025-04-26 22:29:40 +0700

    feat: add Seat, SeatMapNormal, SeatMapSleep component and styles

---
+++ fe/admin/src/components/Seat.tsx
@@ -0,0 +1,48 @@
+import seatActive from "../assets/seat_active.svg";
+import seatDisabled from "../assets/seat_disabled.svg";
+import seatBooked from "../assets/seat_booked.svg";
+import styles from "../styles/seat.module.scss";
+
+export type SeatType = {
+  id?: number;
+  position: string;
+  status: "available" | "booked" | "unavailable";
+  tripId?: number;
+  floor?: "top" | "bottom";
+};
+interface SeatProps {
+  seatValue: SeatType;
+  useStatus: "booked" | "unavailable";
+  onSelected: (item: SeatType) => void;
+}
+
+const Seat: React.FC<SeatProps> = ({ seatValue, useStatus, onSelected }) => {
+  const { position, status } = seatValue;
+
+  const handleToggleStatus = () => {
+    let newStatus;
+    if (status === "available") {
+      newStatus = useStatus;
+    } else if (["booked", "unavailable"].includes(useStatus)) {
+      console.log(1);
+      newStatus = "available";
+    }
+    const updateSeat: SeatType = { ...seatValue, status: newStatus };
+    onSelected(updateSeat);
+  };
+
+  return (
+    <div className={styles.seat} onClick={handleToggleStatus}>
+      <span className={styles.content}>{position}</span>
+      <img
+        src={`${
+          status === "available" ? seatActive : status === "booked" ? seatBooked : seatDisabled
+        }`}
+        className={styles.img}
+        alt={`img${status}`}
+      />
+    </div>
+  );
+};
+
+export default Seat;
