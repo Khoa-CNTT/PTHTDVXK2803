9c6e1529646a717258428e2267640e9d1d7a3341
ngovanlan15 <lanngo1592003@gmail.com>
2025-05-15 21:37:44 +0700

    update regster BE

--- be/service/src/services/customer.service.ts
+++ be/service/src/services/customer.service.ts
@@ -38,7 +38,7 @@ export class CustomerService {
     return new Promise(async (resolve, reject) => {
       try {
         const { email, password, fullName, confirmPassword } = newCustomer;
-
+        
         if (password !== confirmPassword) {
           return resolve({
             status: "ERR",
@@ -62,9 +62,22 @@ export class CustomerService {
         });
 
         const passwordHash = await bcrypt.hash(password, 10);
-        await otpService.insertOtp({ otp, email, passwordHash, fullName, role: "customer" });
-        await sendOtpEmail({ email, otp });
+        const insertOtp = await otpService.insertOtp({
+          otp,
+          email,
+          passwordHash,
+          fullName,
+          role: "customer",
+        });
+        if (insertOtp.data.status === "ERR") {
+          return resolve({
+            status: "ERR",
+            message: insertOtp.data.message,
+          });
+        }
 
+        await sendOtpEmail({ email, otp });
+        console.log("register");
         resolve({
           status: "OK",
           message: "Create OTP success",
@@ -86,7 +99,6 @@ export class CustomerService {
             message: "The OTP code for this email does not exist",
           });
         }
-
         const isValid = await otpService.isValidOtp(otp, checkOtp.otp);
 
         if (!isValid) {
@@ -94,28 +106,39 @@ export class CustomerService {
             status: "ERR",
             message: "Error verifying email",
           });
-        }
-
-        if (isValid && email === checkOtp.email) {
-          const sql = `insert into user (email, password, fullName, role) values (?, ?, ?, ?)`;
+        } else {
+          const sql = `insert into user (email, full_name, password, role) values (?, ?, ?, ?)`;
           const values = [checkOtp.email, checkOtp.fullName, checkOtp.password, "customer"];
 
           const [rows] = (await this.db.execute(sql, values)) as [ResultSetHeader];
 
           if (rows.affectedRows > 0) {
+            const detailCustomer = {
+              email: checkOtp.email,
+              fullName: checkOtp.fullName,
+              role: "customer",
+            };
+
             const access_token = generalAccessToken({ id: email, role: "customer" });
             const refresh_token = generalRefreshToken({ id: email, role: "customer" });
+            const expirationTime = Date.now() + 60 * 60 * 1000;
 
             return resolve({
               status: "OK",
               message: "Register success",
+              data: detailCustomer,
               access_token,
               refresh_token,
+              expirationTime,
             });
           }
         }
       } catch (error) {
-        reject(error);
+        console.log("error", error);
+        reject({
+          status: "ERR",
+          message: "Error verifying email",
+        });
       }
     });
   }
